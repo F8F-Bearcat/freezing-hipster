2326ac1636daa159225973f0aeecde7a7d6b5556
Andy DeBaets <debaets@hotmail.com>
2013-07-29 03:34:59 +0000

    read from url working w http://url on command line

--- grader.js
+++ grader.js
@@ -26,6 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+var URL_DEFAULT = "www.lctips.com"
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,6 +58,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtmlFile2 = function(some_html, checksfile) {
+    $ = cheerio.load(some_html);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +79,34 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to url', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+//    console.log('look at command line parameters')
+//    if (program.checks) console.log('checks flag is %s ', program.checks);
+//    if (program.file) console.log('file flag is %s  ', program.file);
+//    if (program.url) console.log('url flag is %s ', program.url);
+//    console.log('end of command line parameter checks');
+
+    var flg = false;
+    process.argv.forEach(function(val, index, array){
+	if (val === '--url') flg = true;   // console.log(index + ': ' + val);
+    });
+
+    if (flg === false) {
+       var checkJson = checkHtmlFile(program.file, program.checks);
+       var outJson = JSON.stringify(checkJson, null, 4);
+       console.log(outJson);
+    }else{
+       rest.get(program.url).on('complete', function(result) {	
+          $ = cheerio.load(result)
+	  var checkJson = checkHtmlFile2($.html(), program.checks);
+          var outJson = JSON.stringify(checkJson, null, 4);
+          console.log(outJson);
+          });   //	console.log('flag is ', flg);
+    }
+	
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
